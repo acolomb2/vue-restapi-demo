f99ced1a72089ed68614c41e3201f9076c08fbf0
Andres Colombon <acolomb2@stu.southtexascollege.edu>
2023-10-29 23:14:02 -0500

    Made site more boring & added back button

--- src/components/BaseCard.vue
+++ src/components/BaseCard.vue
@@ -17,14 +17,14 @@ const props = defineProps({
   <RouterLink
     v-if="props.character.multiverseid"
     :to="`/cards/${props.character.multiverseid}`"
-    class="rounded-lg bg-white shadow-lg"
+    class="rounded-lg bg-white shadow-lg "
   >
     <img
-      class="w-full rounded-t-lg object-cover"
+      class="w-40 rounded-t-lg object-cover ml-auto mt-2 mr-auto hover:w-full hover:mt-0 transition-all"
       :src="props.character.imageUrl"
     />
     <div class="p-4">
-      <h2 class="text-1xl font-semibold text-gray-800">
+      <h2 class="text-1xl font-semibold text-black">
         {{ props.character.name }}
       </h2>
     </div>
--- src/views/CharacterDetails.vue
+++ src/views/CharacterDetails.vue
@@ -19,8 +19,12 @@ onUnmounted(() => {
 
 <template>
   <main
-    class="min-h-screen bg-gradient-to-r from-green-900 to-blue-700 py-8 text-white"
+    class="min-h-screen bg-gradient-to-b from-white to-gray-500 from-green-900 to-blue-700 py-8 text-white"
   >
+ 
+ <button class="text-white cursor-pointer bg-black text-white ml-5 rounded-md hover:bg-gray-300 hover:text-black transition-all active:text-yellow-500" onclick="history.back()">‎ &lt; Go Back ‎ </button>
+
+  
     <div
       v-if="currentCharacter"
       class="flex flex-col items-center justify-center gap-6"
@@ -30,7 +34,7 @@ onUnmounted(() => {
         :alt="currentCharacter.card.name"
       />
       <h1 class="text-white-800 text-6xl font-bold">
-        Hi, I'm {{ currentCharacter.card.name }}
+        {{ currentCharacter.card.name }}
       </h1>
       <pre>{{ currentCharacter.card.originalText }}</pre>
     </div>
